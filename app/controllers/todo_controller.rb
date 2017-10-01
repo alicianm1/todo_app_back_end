ccd1916d53086150147f59b15f89017a3fdcbaa4
Alicia Mitchell <alicianm9@gmail.com>
2017-10-01 16:01:58 +0000

    these are my changes

---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,42 @@
+class TodoController<ApplicationController
+    def index 
+    end
+    def show 
+        todo_id= params[:id]
+        #if user types/todo/show/1 in url
+        #make @todo_description to be "Finish U.S History paper"
+        # and make @pomodoro_estimate=4
+        if todo_id=='1'
+            @todo_description= "precalc assignment"
+            @todo_pomodoro_estimate= 4
+         #if user types/todo/show/2 in url
+        #make @todo_description to be "wash dishes"
+        # and make @pomodoro_estimate=3
+        elsif todo_id=='2'
+            @todo_description= "wash dishes"
+            @todo_pomodoro_estimate=3
+         #if user types/todo/show/3 in url
+        #make @todo_description to be "do laundry"
+        # and make @pomodoro_estimate=5
+        elsif todo_id=='3'
+            @todo_description= "sweep"
+            @todo_pomodoro_estimate=5
+         #if user types/todo/show/4 in url
+        #make @todo_description to be "cook"
+        # and make @pomodoro_estimate=3
+        elsif todo_id== '4'
+            @todo_description= "clean the bathroom"
+            @todo_pomodoro_estimate=3
+         #if user types/todo/show/5 in url
+        #make @todo_description to be "clean room"
+        # and make @pomodoro_estimate=4
+        elsif todo_id== '5'
+            @todo_description= "clean room"
+            @todo_pomodoro_estimate= 4
+        end
+    
+        
+        
+    end
+    
+end
